35e7911c9f78cc5e7570e8cd9ce10f6d8843455f
J.M <kabugijeff@gmail.com>
2023-05-01 16:27:50 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
+import { UserProvider } from "./context/user";
+import { ThemeProvider } from "../context/Theme";
+
 
 function App() {
   const [theme, setTheme] = useState("dark");
   const [user, setUser] = useState(null);
   return (
     <main className={theme}>
-      <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
-      <Profile theme={theme} user={user} />
+      <UserProvider>
+        <ThemeProvider>
+          <Header />
+          <Profile />
+        </ThemeProvider>
+      </UserProvider>
     </main>
   );
 }
